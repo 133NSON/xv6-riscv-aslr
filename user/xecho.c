bb18ec6b9c98e7faa79c277661982cec0180c675
Johnny Bui <me@johnnybui.com>
2019-10-28 01:44:20 -0400

    add xecho support for stdin read since commands can only be 100 long

--- user/xecho.c
+++ user/xecho.c
@@ -10,11 +10,30 @@ const char hex[] = "0123456789abcdef";
 
 int main(int argc, char *argv[])
 {
-  for (int j = 0; j < strlen(argv[1]); j += 2)
+  char* input;
+
+  if (argc == 1) {
+    // no arguments were provided, read from stdin
+    char buffer[1000];
+    memset(buffer, 0, 1000);
+    input = buffer;
+    gets(buffer, 1000);
+    // replace newline character
+  } else {
+    input = argv[1];
+  }
+  for (int j = 0; j < strlen(input); j += 2)
   {
-    char upper = (char)(strchr(hex, argv[1][j]) - hex);
-    char lower = (char)(strchr(hex, argv[1][j + 1]) - hex);
-    char byte = upper<<4 | lower;
+    char* upper = strchr(hex, input[j]);
+    if (!upper) break; // break if input is int valid hex
+    char* lower = strchr(hex, input[j + 1]);
+    if (!lower) break; // break if input is not valid hex
+
+    // since strchr returns pointer, subtract from base pointer to find index
+    char upperBits = (char) (upper - hex) << 4;
+    char lowerBits = (char) (lower - hex);
+
+    char byte = upperBits | lowerBits;
     write(1, &byte, 1);
   }
   write(1, "\n", 1);
